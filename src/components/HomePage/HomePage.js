f81929857f49d9b27b0714676d7e5996dfa85eca
walaaelgenidy <45464802+walaaelgenidy@users.noreply.github.com>
2020-08-04 21:02:59 +0200

    create-home-page'

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,44 @@
+import React from 'react' ;
+import './HomePage.scss';
+const HomePage = ()=>(
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>hats</h1>
+                    <span className='subtitle'>shop now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>jackets</h1>
+                    <span className='subtitle'>shop now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>pants</h1>
+                    <span className='subtitle'>shop now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>women</h1>
+                    <span className='subtitle'>shop now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>men</h1>
+                    <span className='subtitle'>shop now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+);
+
+export default HomePage;
+
+
+
