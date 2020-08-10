ebdade044420c954488d2dfe700a457265a413ee
walaaelgenidy <45464802+walaaelgenidy@users.noreply.github.com>
2020-08-10 23:31:28 +0200

    updating app by adding shopping page and collection previw component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import React from 'react';
 import './App.css';
 import {Route , Switch} from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
+import ShopPage from './pages/shop/shop';
 
-const HatsPage=()=>(
-<div>
-  <h1>hats</h1>
-</div>
-);
 
 
 function App() {
@@ -15,7 +11,7 @@ function App() {
     <div>
       <Switch>
      <Route exact path='/' component={HomePage} />
-     <Route path='/hats' component={HatsPage} />
+     <Route path='/shoppage' component={ShopPage} />
       </Switch>
     </div>
   );
